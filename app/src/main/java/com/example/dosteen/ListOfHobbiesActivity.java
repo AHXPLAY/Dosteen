fd09f10bfff3a88ab8808d0703d1207ec14d9cb1
AHXPLAY <48548324+AHXPLAY@users.noreply.github.com>
2020-05-05 12:10:53 +0900

    Test

---
+++ app/src/main/java/com/example/dosteen/ListOfHobbiesActivity.java
@@ -0,0 +1,15 @@
+package com.example.dosteen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AppOpsManager;
+import android.os.Bundle;
+
+public class ListOfHobbiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_hobbies);
+    }
+}
